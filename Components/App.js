f35051ee39c09b7ad7bb5a3d0837e3399b369eed
mini-militant <shailesh.coolcalm007@gmail.com>
2019-08-09 12:42:33 +0530

    1 file modified, 1 added

--- Components/App.js
+++ Components/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import {Route} from 'react-router-dom'
 import HomePage from './Home/HomePage'
 import AboutPage from './About/AboutPage'
-
+import Header from './Common/Header'
 function App(){
   return(
     <div className="container-fluid">
+      <Header/>
       <Route exact path='/' component = {HomePage}/>
       <Route path='/about' component = {AboutPage}/>
     </div>
