4c09edf5e857adc830f9851d5bb33a4da293466c
mini-militant <shailesh.coolcalm007@gmail.com>
2019-08-09 12:51:55 +0530

    errorPage implemented

--- Components/App.js
+++ Components/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import {Route} from 'react-router-dom'
+import {Route,Switch} from 'react-router-dom'
 import HomePage from './Home/HomePage'
 import AboutPage from './About/AboutPage'
 import Header from './Common/Header'
-function App(){
+import PageNotFound from './PageNotFound'
+
+function App(){ 
   return(
     <div className="container-fluid">
       <Header/>
+      <Switch >  //switch allows us to declare that only one route should match.
       <Route exact path='/' component = {HomePage}/>
       <Route path='/about' component = {AboutPage}/>
+      <Route component={PageNotFound}/>
+      </Switch>
+{/* Switch in routes acts as a switch in js. notice that there is no path for PageNotFound file as when none of the routes match, this page will execute.*/}
+
     </div>
   )
 }
---
+++ Components/PageNotFound.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+const PageNotFound =() =>(<h1>Oops! Page does not exist.</h1>)
+export default PageNotFound
