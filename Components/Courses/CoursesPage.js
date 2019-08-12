05b5e5aecec7d595b1755dbdfb36a1c457b7be9a
mini-militant <shailesh.coolcalm007@gmail.com>
2019-08-12 10:43:35 +0530

    1 file modified

--- Components/Courses/CoursesPage.js
+++ Components/Courses/CoursesPage.js
@@ -1,12 +1,41 @@
 import React from 'react'
 
 class CoursesPage extends React.Component{
+  
+  constructor(props){
+    super(props);
+    this.state={
+      course:{
+        title:""
+      }
+    }
+    this.handleChange=this.handleChange.bind(this)
+  }
+
+  handleChange(event){
+    const course={ ...this.state.course,title:event.target.value};
+    //the above code creates a copy of course object with an updated title.
+    //with object spread, values on the right override those on the left.
+    this.state({
+      course:course
+    });
+    event.preventDefault();
+  }
+
 
   render(){
     return(
-      <div>
-      <h1>Courses Page</h1>
-      </div>
+      <form>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        
+        <input type="text" 
+        onChange={this.handleChange}
+        value={this.state.course.title}
+        />
+
+        <input type="submit" value="Save"/>
+      </form>
     )
   }
 }
