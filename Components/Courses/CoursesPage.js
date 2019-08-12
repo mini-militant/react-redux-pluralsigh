1c64c6f6970a41daf1b8efd62f413e4bcb601e58
mini-militant <shailesh.coolcalm007@gmail.com>
2019-08-12 12:40:42 +0530

    1 file modified

--- Components/Courses/CoursesPage.js
+++ Components/Courses/CoursesPage.js
@@ -20,13 +20,17 @@ class CoursesPage extends React.Component{
     this.setState({
       course:course
     });
- 
-  }
+   }
+
+  handleSubmit=(event)=>{
+    event.preventDefault();
+    alert(this.state.course.title)
+  } 
 
 
   render(){
     return(
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
         <h3>Add Course</h3>
         
